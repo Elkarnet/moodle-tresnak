206c7b02b25a16bd7b647c98ec8963df8df2ad3d
Pedro Arreitunandia <parreitu@protonmail.com>
2022-07-04 15:20:12 +0200

    Create reseteatzeko_kategoria_ikastaroak.py

---
+++ moosh/adibideak/reseteatzeko_kategoria_ikastaroak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+reseteatzeko_kategoria_zerrenda = [
+
+# Arloa: Ez da scriptean erabiltzen, guk kategoria errezago identifikatzeko erabiltzen da
+# goiko_kategoria: Kategoria honen azpian dauden ikastaro guztiak reseteatuko dira
+# kudeatzaileak: Zerrenda honetan daude erabiltzaileak (erabiltzaile IDa erabili) kudeatzaile rola jasoko dute kategoria horren azpian dauden ikastaro guztietan.
+# hauek_ez_reseteatu: Kategoria baten azpian dauden ikastaro batzuk reseteatu nahi ez baditugu, zerrenda honetan jarri ikastaro hauen IDak.
+
+# Beheko lerroak deskomentatu eta zure beharretara egokitu
+
+# {"arloa":"A-Zikloa", "goiko_kategoria":'9', 'Kudeatzaileak':['160','229'], 'hauek_ez_reseteatu':['66', '80', '116', '139', '159']},
+# {"arloa":"B-Zikloa", "goiko_kategoria":'24', 'Kudeatzaileak':['218'], 'hauek_ez_reseteatu':['235']},
+# {"arloa":"C-Zikloa", "goiko_kategoria":'35', 'Kudeatzaileak':['188'], 'hauek_ez_reseteatu':[]},
+
+]
